36bb91439975932e0376bc0d3201b7417bc39764
Alexys Santiago <SuxPorT@hotmail.com>
2024-01-01 22:43:46 -0300

    feat: add project #001

---
+++ 001-bg-video/README.md
@@ -0,0 +1,3 @@
+# 001 - Unleashing the Power of Video: Create Stunning Website Backgrounds with HTML5 and CSS3
+
+Link reference: https://youtu.be/pbyMcxnlYPQ?si=u4qClQP09Xy3IrWj
---
+++ 001-bg-video/script.js
@@ -0,0 +1,11 @@
+function explore() {
+  fetch("./assets/Credits.txt")
+    .then((res) => res.text())
+    .then((text) => {
+      const urlRegex = /(https?:\/\/[^\s]+)/;
+      const match = text.match(urlRegex);
+      const link = document.getElementById('link');
+      link.href = match[0];
+    })
+    .catch((e) => console.log(e));
+}
